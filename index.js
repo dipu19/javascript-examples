fbf9e53ce9adf53fbcfb2f038b73b0dcff8f4224
dipu19 <50631686+dipu19@users.noreply.github.com>
2022-07-11 22:18:46 +0530

    javascript function to print helloworld

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
-function example (){
-  return new Promise(function(resolve, reject){
-    const error = false
-    if(error){
-      console.log("eroor occured")
-      reject();
-    }
-    else{
-      console.log("sucessfull")
-      setTimeout(()=>{console.log("inside time out function ")},5000)
-      resolve()
-    }
-  })
+function helloWorld(){
+  console.log("hello world")
 }
-example().then(()=>console.log("inside resolve")).catch(()=>console.log("inside reject"))
+helloWorld()
