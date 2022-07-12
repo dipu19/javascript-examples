c39be8024b8decf5dc92bd579b49378df24bdd77
dipu19 <50631686+dipu19@users.noreply.github.com>
2022-07-12 10:01:44 +0530

    Checking number is even or odd

---
+++ evenOrOdd.js
@@ -0,0 +1,11 @@
+function check(num1){
+    if(num1 % 2 === 0)
+    {
+        console.log("number is even ")
+    }
+    if(num1 % 2 !== 0){
+        console.log("number is odd")
+    }
+
+}
+check(4)
